1c602bef6fc9bf8d7f1b826ef38d30ed40dc8870
Avhira <yusupuhamka@gmail.com>
2024-04-16 17:22:45 +0700

    delete profileBarLink

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import Back from '../component/Back';
-import ProfileBarLinks from '../component/Profile/ProfileBarLinks';
 import DataDescription from '../component/Profile/DataDescription';
 import Technology from '../component/Profile/Technology';
 import Contact from '../component/Profile/Contact';
@@ -11,7 +10,17 @@ export default function MyProfile() {
   return (
     <div className="background-profile">
       <Back />
-      <ProfileBarLinks showType={showType} setShowType={setShowType} />
+      <div className="profile-bar">
+        <button style={{ backgroundColor: showType === 'profile' ? '#0700dd' : null }} onClick={() => setShowType('profile')}>
+          PROFILE
+        </button>
+        <button style={{ backgroundColor: showType === 'contact' ? '#0700dd' : null }} onClick={() => setShowType('contact')}>
+          CONTACT
+        </button>
+        <button style={{ backgroundColor: showType === 'tech' ? '#0700dd' : null }} onClick={() => setShowType('tech')}>
+          TECHNOLOGY
+        </button>
+      </div>
       <section className="profile">{showType === 'profile' ? <DataDescription /> : showType === 'contact' ? <Contact /> : <Technology />}</section>
     </div>
   );
